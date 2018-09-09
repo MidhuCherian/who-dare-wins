a840149740abbef8b4b4dff106cdd45771af9860
MidhuCherian <midhucherian91@gmail.com>
2018-09-09 14:19:26 +1200

    msg

---
+++ files-win/who-dare-wins/public/javascripts/entries.js
@@ -0,0 +1,86 @@
+// We want to wait until the page is loaded so that the form is there to bind the submit listener to.
+window.addEventListener('load', function () {
+    //initialise firestore
+    var db = firebase.firestore();
+    var settings = { timestampsInSnapshots: true };
+    db.settings(settings);
+    var entriesCollection = db.collection('entries');
+    listenForEntries();
+
+    function storeEntry(entry) {
+        entriesCollection.add(entry);
+    }
+
+    // Keep a local cache of the firestore data for easy access
+    var entries = [];
+
+    function listenForEntries() {
+        entriesCollection
+            .onSnapshot(function (querySnapshot) {
+                entries = querySnapshot.docs
+                    .map(function (doc) {
+                        return doc.data();
+                    })
+                    .sort(function (a, b) {
+                        return a.timestamp > b.timestamp ? 1 : -1;
+                    });
+
+                resetEntries();
+                entries.forEach(function (entry) {
+                    updateEntryList(entry);
+                });
+            });
+    }
+
+    function getRandomEntry() {
+        var randomIndex = Math.floor(Math.random() * entries.length);
+        return entries[randomIndex];
+    }
+
+    function pickAWinner() {
+        var winner = getRandomEntry();
+        var winningText = 'Congratulations ' + winner.name + ', you have won a Frontend Masters subscription!';
+        document.getElementById('winner').innerText = winningText;
+    }
+
+    function getDrawButton() {
+        return document.getElementById('draw_button');
+    }
+    function getEntryForm() {
+        return document.getElementById('entry_form');
+    }
+    function resetEntries() {
+        var ol = document.getElementById('entry_list');
+        ol.innerHTML = "";
+    }
+
+    function addEntry(event) {
+        // Stop the form from reloading the page
+        event.preventDefault();
+
+        var formData = new FormData(getEntryForm());
+        var now = new Date();
+        var entry = {
+            timestamp: now.getTime(),
+            name: formData.get('name'),
+            email: formData.get('email')
+        };
+
+        storeEntry(entry);
+    }
+
+    function updateEntryList(entry) {
+        // Create an object/model to represent the entry
+        // var entry = { name: formData.get('name'), email: formData.get('email') }
+
+        var ol = document.getElementById('entry_list');
+        var entryText = document.createTextNode(entry.name + ' (' + entry.email + ')');
+        var li = document.createElement('li');
+
+        li.appendChild(entryText);
+        ol.appendChild(li);
+    }
+
+    getEntryForm().addEventListener('submit', addEntry);
+    getDrawButton().addEventListener('click', pickAWinner);
+});
